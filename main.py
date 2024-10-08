5e9687dc707bc76a12b4f46b8753ab7fe1142224
angele5 <angele.ramauge@dauphine.eu>
2024-10-08 12:45:31 +0200

    V1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pettingzoo.butterfly import knights_archers_zombies_v10
 
-env = knights_archers_zombies_v10.env(render_mode="human")
+env = knights_archers_zombies_v10.env()
 env.reset(seed=42)
 
 for agent in env.agent_iter():
@@ -8,6 +8,7 @@ for agent in env.agent_iter():
     if termination or truncation:
         action = None
     else:
+        print(env.observation_space(agent))
         # this is where you would insert your policy
         action = env.action_space(agent).sample()
 
